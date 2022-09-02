7a34dda66145018a4d4736f628e40a2dd9fd5ca2
Lakshay Meghlan <lakshaymeghlan6969@gmail.com>
2022-09-02 10:25:27 +0530

    add joi validation in cart and wishlist

--- src/Routes/cartRoutes.js
+++ src/Routes/cartRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
-// import { addToCart,deleteCart,cartProduct,AllCartProduct } from "../Controllers/cartController";
 import cartCtrl from "../Controllers/cartController";
 import _ from "lodash";
+import { validationMiddleware } from "../helper/validation";
 
 const router = express.Router();
 
-router.post("/addToCart", cartCtrl.addToCart);
-router.delete("/delete/:id", cartCtrl.deleteCart);
-router.get("/cartProduct/:id", cartCtrl.cartProduct);
-router.get("/AllCartProduct", cartCtrl.AllCartProduct);
+router.post("/addToCart",validationMiddleware, cartCtrl.addToCart);
+router.delete("/delete/:id",validationMiddleware, cartCtrl.deleteCart);
+router.get("/cartProduct/:id",validationMiddleware, cartCtrl.cartProduct);
+router.get("/AllCartProduct",validationMiddleware, cartCtrl.AllCartProduct);
 
 export default router;
--- src/Routes/whishlistRoutes.js
+++ src/Routes/whishlistRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import _ from "lodash";
 import wishlistCtrl from "../Controllers/wishlistController";
-// import { validationMiddleware } from "../Controllers/wishlistController";
+import { validationMiddleware } from "../helper/validation";
 
 
 const router = express.Router();
 
-router.post("/create",wishlistCtrl.createWishlist);
-router.delete("/delete/:id",wishlistCtrl.deleteWishlist);
-router.get("/allWishlistProduct",wishlistCtrl.allWishlistProduct)
-router.get("/wishlist/:id",wishlistCtrl.getWishlist);
+router.post("/create",validationMiddleware,wishlistCtrl.createWishlist);
+router.delete("/delete/:id",validationMiddleware,wishlistCtrl.deleteWishlist);
+router.get("/allWishlistProduct",validationMiddleware,wishlistCtrl.allWishlistProduct)
+router.get("/wishlist/:id",validationMiddleware,wishlistCtrl.getWishlist);
 
 export default router;
