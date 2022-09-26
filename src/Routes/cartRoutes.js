c1dc426feb7065b2ac48be0a47c4b90e04cbb23d
Lakshay Meghlan <lakshaymeghlan6969@gmail.com>
2022-09-27 02:33:31 +0530

    completed API checkbox in cart

--- src/Routes/cartRoutes.js
+++ src/Routes/cartRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/addToCart",validationMiddleware, cartCtrl.addToCart); 
 router.delete("/delete/:id/product/:productId",validationMiddleware,cartCtrl.deleteProduct);
-router.patch("/deleteselected",wishlistCtrl.deleteMultipleProduct);
+router.patch("/deleteselected",cartCtrl.deleteMultipleProduct);
 router.delete("/delete/:id",validationMiddleware, cartCtrl.deleteCart);
 router.get("/cartProduct/:userId",validationMiddleware, cartCtrl.cartProduct);
 router.get("/AllCartProduct",validationMiddleware, cartCtrl.AllCartProduct);
